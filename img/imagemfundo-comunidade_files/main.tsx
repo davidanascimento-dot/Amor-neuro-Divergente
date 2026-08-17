d12228c137f9af7dc4ffffd14ba4f52b9e3eecb5
davidanascimento-dot <david.a.nascimento@ba.estudante.senai.br>
2026-08-17 14:56:12 -0300

    comit

---
+++ img/imagemfundo-comunidade_files/main.tsx
@@ -0,0 +1,17 @@
+import __vite__cjsImport0_react from "/node_modules/.vite/deps/react.js?v=b81b5bea"; const React = __vite__cjsImport0_react;
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=937e7567"; const ReactDOM = __vite__cjsImport1_reactDom_client;
+import App from "/src/App.tsx";
+import "/src/index.css";
+var _jsxFileName = "/workspaces/default/code/src/main.tsx";
+import __vite__cjsImport4_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=b81b5bea"; const _jsxDEV = __vite__cjsImport4_react_jsxDevRuntime["jsxDEV"];
+ReactDOM.createRoot(document.getElementById("root")).render(/* @__PURE__ */ _jsxDEV(React.StrictMode, { children: /* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 8,
+	columnNumber: 5
+}, this) }, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 7,
+	columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJtYXBwaW5ncyI6IkFBQUEsT0FBTyxXQUFXO0FBQ2xCLE9BQU8sY0FBYztBQUNyQixPQUFPLFNBQVM7QUFDaEIsT0FBTzs7O0FBRVAsU0FBUyxXQUFXLFNBQVMsZUFBZSxPQUFPLENBQUUsQ0FBQyxPQUNwRCx3QkFBQyxNQUFNLFlBQVAsWUFDRSx3QkFBQyxLQUFELEVBQU87Ozs7VUFDVTs7OztTQUNwQiIsIm5hbWVzIjpbXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJ2ZXJzaW9uIjozLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnXG5pbXBvcnQgUmVhY3RET00gZnJvbSAncmVhY3QtZG9tL2NsaWVudCdcbmltcG9ydCBBcHAgZnJvbSAnLi9BcHAnXG5pbXBvcnQgJy4vaW5kZXguY3NzJ1xuXG5SZWFjdERPTS5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdyb290JykhKS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcbilcbiJdfQ==
